0f12f36a29ed56aac5e1e14155b02a9a998b1126
Manju31051996 <bbelagavi6@gmail.com>
2022-12-07 20:22:40 +0530

    [Manjunath S B] added the welcome message to address book system file

---
+++ src/com/addressbookmgmtsystem/AddressBookMgmtSys.java
@@ -0,0 +1,8 @@
+package com.addressbookmgmtsystem;
+
+public class AddressBookMgmtSys {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Management System..!");
+
+    }
+}
