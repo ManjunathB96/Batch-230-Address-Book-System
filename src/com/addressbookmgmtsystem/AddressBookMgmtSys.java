6d54abf12f7dc41473a194e32779281652bc2aad
Manju31051996 <bbelagavi6@gmail.com>
2022-12-07 23:09:27 +0530

    [Manjunath S B]added the UC2 file to add contacts to address book management System

--- src/com/addressbookmgmtsystem/AddressBookMgmtSys.java
+++ src/com/addressbookmgmtsystem/AddressBookMgmtSys.java
@@ -1,8 +1,76 @@
 package com.addressbookmgmtsystem;
 
-public class AddressBookMgmtSys {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Management System..!");
+import java.util.Scanner;
 
+public class AddressBookMgmtSys {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Contact[] contacts = new Contact[20];
+
+    public static void main(String[] args) {
+        AddressBookMgmtSys sys = new AddressBookMgmtSys();
+
+        System.out.println("Welcome to Address Book Management System..!");
+        System.out.println();
+
+        sys.addEditDeleteFindContact();
+    }
+
+    public void addEditDeleteFindContact() {
+        boolean isExist = false;
+        while (!isExist) {
+            System.out.println("Please Select Option\n1.addContact");
+            int choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    addContact();
+                    break;
+                default:
+                    System.out.println("Invalid Choice Please Select Valid Option...!");
+
+            }
+        }
+    }
+
+    private static Contact addContact() {
+        Contact contact = new Contact();    //constructor is created in contact class (empty)
+        System.out.println("Enter First Name :");
+        contact.setfName(scanner.nextLine());
+
+        System.out.println("Enter Middle Name :");
+        contact.setmName(scanner.nextLine());
+
+        System.out.println("Enter Last Name :");
+        contact.setlName(scanner.nextLine());
+
+        System.out.println("Enter Residential address :");
+        contact.setAdd(scanner.nextLine());
+
+        System.out.println("Enter City Name  :");
+        contact.setCity(scanner.nextLine());
+
+        System.out.println("Enter State  Name :");
+        contact.setState(scanner.nextLine());
+
+        System.out.println("Enter Country Name :");
+        contact.setCountry(scanner.nextLine());
+
+        System.out.println("Enter PinCode :");
+        contact.setPinCode(scanner.nextInt());
+
+        System.out.println("Enter Phone Number :");
+        contact.setPhoneNum(scanner.nextLong());
+
+        System.out.println("Enter Email Address:");
+        contact.setEmail(scanner.nextLine());
+
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null) {
+                contacts[i] = contact;
+                System.out.println("Contact Added Successfully in Address book system....!");
+                return contact;
+            }
+        }
+        System.out.println("Contact List is Full No Space For New Contacts");
+        return contact;
     }
 }
--- src/com/addressbookmgmtsystem/Contact.java
+++ src/com/addressbookmgmtsystem/Contact.java
@@ -2,20 +2,108 @@ package com.addressbookmgmtsystem;
 
 public class Contact {
     private String fName;
+    private  String mName;
     private String lName;
     private String add;
     private String city;
     private String state;
+    private String country;
     private int pinCode;
     private long phoneNum;
     private String email;
 
-    public Contact(String fName, String lName, String add, String city, String state, int pinCode, long phoneNum, String email) {
+    public Contact() {
+
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
         this.fName = fName;
+    }
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public long getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(long phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Contact(String fName, String mName,String lName, String add,
+                   String city, String state,String country,
+                   int pinCode, long phoneNum, String email) {
+        this.fName = fName;
+        this.mName = mName;
         this.lName = lName;
         this.add = add;
         this.city = city;
         this.state = state;
+        this.country = country;
         this.pinCode = pinCode;
         this.phoneNum = phoneNum;
         this.email = email;
